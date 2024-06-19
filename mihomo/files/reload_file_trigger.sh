d4ec36c410a6c635581203cfade2ddc1a375d18b
Joseph Mory <morytyann@outlook.com>
2024-06-19 08:41:09 +0000

    refactor upnp leases monitoring, don't reload service when upnp leases is updated, instead update upnp exclusion

--- mihomo/files/reload_file_trigger.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-if (inotifywait -e modify "$1"); then eval "/etc/init.d/$2 reload"; fi
---
+++ mihomo/files/upnp_lease_watcher.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+. /lib/functions.sh
+
+load_config() {
+  config_load upnpd
+  config_get upnp_enabled "config" "enabled" 0
+  config_get upnp_lease_file "config" "upnp_lease_file" "/var/run/miniupnpd.leases"
+}
+
+add_upnp_exclusion() {
+	local timestamp; timestamp=$(date +%s)
+	local lease_expire_at lease_proto lease_src_ip lease_src_port
+
+	while read -r line;	do
+		lease_expire_at=$(echo "$line" | awk -F ':' '{print $5}')
+		local timeout; timeout=$(( lease_expire_at - timestamp ))
+		if [ "$timeout" -le 0 ]; then
+			continue
+		fi
+		lease_proto=$(echo "$line" | awk -F ':' '{print tolower($1)}')
+		lease_src_ip=$(echo "$line" | awk -F ':' '{print $3}')
+		lease_src_port=$(echo "$line" | awk -F ':' '{print $4}')
+		nft add element ip mihomo upnp_exclusion \{ "$lease_proto" . "$lease_src_ip" . "$lease_src_port" timeout "${timeout}s" \}
+	done < "$upnp_lease_file"
+}
+
+load_config
+if [ "$upnp_enabled" == 0 ]; then return fi
+
+while true; do
+  load_config
+  add_upnp_exclusion
+  inotifywait -e create,modify --include $(basename "$upnp_lease_file") $(dirname "$upnp_lease_file")
+done
