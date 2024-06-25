1f1768e5796155a9869715973a0c90045ec9a4f8
Joseph Mory <morytyann@outlook.com>
2024-06-25 05:57:06 +0000

    add option for dns hijack, when disabled, dns request will not redirect to core

--- mihomo/files/migrate.sh
+++ mihomo/files/migrate.sh
@@ -3,4 +3,7 @@
 # add mihomo.config.mixin
 mixin=$(uci -q get mihomo.config.mixin); [ -z "$mixin" ] && uci set mihomo.config.mixin=1
 
+# add mihomo.proxy.dns_hijack
+dns_hijack=$(uci -q get mihomo.proxy.dns_hijack); [ -z "$dns_hijack" ] && uci set mihomo.proxy.dns_hijack=1
+
 uci commit mihomo
