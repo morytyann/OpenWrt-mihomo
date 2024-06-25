0467034edff118226a3f3f7ab35e39f05904c733
Joseph Mory <morytyann@outlook.com>
2024-06-25 06:41:14 +0000

    update migrate

--- mihomo/files/migrate.sh
+++ mihomo/files/migrate.sh
@@ -6,4 +6,7 @@ mixin=$(uci -q get mihomo.config.mixin); [ -z "$mixin" ] && uci set mihomo.confi
 # add mihomo.proxy.dns_hijack
 dns_hijack=$(uci -q get mihomo.proxy.dns_hijack); [ -z "$dns_hijack" ] && uci set mihomo.proxy.dns_hijack=1
 
+# delete mihomo.proxy.routing_mark
+routing_mark=$(uci -q get mihomo.proxy.routing_mark); [ -n "$routing_mark" ] && uci del mihomo.proxy.routing_mark
+
 uci commit mihomo
