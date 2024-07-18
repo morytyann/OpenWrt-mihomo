9f02183d29e08eb9c9a5854eb5a8ebeeb48ff622
Joseph Mory <morytyann@outlook.com>
2024-07-18 09:45:03 +0000

    compatible with mwan
    1. add mihomo.proxy.wan_interfaces option
    2. add mihomo.mixin.outbound_interface option

--- mihomo/files/migrate.sh
+++ mihomo/files/migrate.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+. /lib/functions/network.sh
+
 # delete mihomo.proxy.routing_mark
 routing_mark=$(uci -q get mihomo.proxy.routing_mark); [ -n "$routing_mark" ] && uci del mihomo.proxy.routing_mark
 
@@ -10,7 +12,7 @@ mixin=$(uci -q get mihomo.config.mixin); [ -z "$mixin" ] && uci set mihomo.confi
 dns_hijack=$(uci -q get mihomo.proxy.dns_hijack); [ -z "$dns_hijack" ] && uci set mihomo.proxy.dns_hijack=1
 
 # add mihomo.mixin.log_level
-log_level=$(uci -q get mihomo.mixin.log_level); [ -z "$log_level" ] && uci set mihomo.mixin.log_level='info'
+log_level=$(uci -q get mihomo.mixin.log_level); [ -z "$log_level" ] && uci set mihomo.mixin.log_level=info
 
 # add mihomo.mixin.authentication
 authentication=$(uci -q get mihomo.mixin.authentication); [ -z "$authentication" ] && {
@@ -32,6 +34,15 @@ log=$(uci -q get mihomo.log); [ -z "$status" ] && uci set mihomo.log=log
 
 # add mihomo.proxy.bypass_china_mainland_ip
 bypass_china_mainland_ip=$(uci -q get mihomo.proxy.bypass_china_mainland_ip); [ -z "$bypass_china_mainland_ip" ] && uci set mihomo.proxy.bypass_china_mainland_ip=0
+
+# get wan interface
+network_find_wan wan_dev
+
+# add mihomo.proxy.wan_interfaces
+wan_interfaces=$(uci -q get mihomo.proxy.wan_interfaces); [ -z "$wan_interfaces" ] && uci add_list mihomo.proxy.wan_interfaces="$wan_dev"
+
+# add mihomo.mixin.outbound_interface
+outbound_interface=$(uci -q get mihomo.mixin.outbound_interface); [ -z "$outbound_interface" ] && uci set mihomo.mixin.outbound_interface="$wan_dev"
 
 # commit
 uci commit mihomo
