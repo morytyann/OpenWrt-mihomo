dfa9cff62c427ea9220378c596bd3a4770409b08
Joseph Mory <morytyann@outlook.com>
2024-09-06 23:05:42 +0800

    Update mihomo.js

--- luci-app-mihomo/htdocs/luci-static/resources/tools/mihomo.js
+++ luci-app-mihomo/htdocs/luci-static/resources/tools/mihomo.js
@@ -105,7 +105,7 @@ return baseclass.extend({
             } else {
                 url = `http://${window.location.hostname}:${apiPort}/ui/?host=${window.location.hostname}&hostname=${window.location.hostname}&port=${apiPort}&secret=${apiSecret}`;
             }
-            window.open(url, '_blank');
+            setTimeout(() => window.open(url, '_blank'), 0);
         } else {
             alert(_('Service is not running.'));
         }
